7e9758c4c62b83295cc4d51199917742b03da58b
gregb79 <gregbassett@bigpond.com>
2025-03-11 00:19:31 +1000

    Update cc1101.h

--- components/cc1101/cc1101.h
+++ components/cc1101/cc1101.h
@@ -3,6 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/spi/spi.h"
+#include <vector>
 
 namespace esphome {
 namespace cc1101 {
