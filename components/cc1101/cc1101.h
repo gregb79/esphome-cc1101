5d60c010258d8deb63eeae9e496fdcdf4d62f1bc
gregb79 <gregbassett@bigpond.com>
2025-03-11 01:21:49 +1000

    Update cc1101.h

--- components/cc1101/cc1101.h
+++ components/cc1101/cc1101.h
@@ -95,12 +95,12 @@ public:
 
   void begin_tx();
   void end_tx();
-  std::vector<int> get_data();
+  std::vector<int> get_data(int id0, int id1, int instruction, int mode);
   void set_spa_electric_id0_input(int value);
   void set_spa_electric_id1_input(int value);
   void set_spa_electric_instruction_input(int value);
   void set_spa_electric_mode_select(int value);
-  std::vector<int> get_data(int id0, int id1, int instruction, int mode);
+
 };
 
 template<typename... Ts> class BeginTxAction : public Action<Ts...>, public Parented<CC1101>
