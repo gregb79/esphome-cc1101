5842c510430a51e92cfef3e6ba9b7b4d586552a4
gregb79 <gregbassett@bigpond.com>
2025-07-17 16:17:59 +1000

    Update sensor.py

--- components/cc1101/sensor.py
+++ components/cc1101/sensor.py
@@ -80,10 +80,10 @@ CC1101_ACTION_SCHEMA = maybe_simple_id(
 GET_DATA_ACTION_SCHEMA = cv.Schema(
     {
         cv.Required(CONF_ID): cv.use_id(CC1101),
-        cv.Required("id0"): cv.int_,,
-        cv.Required("id1"): cv.int_,,
+        cv.Required("id0"): cv.int_,
+        cv.Required("id1"): cv.int_,
         cv.Required("instruction"): cv.int_,
-        cv.Required("mode"): cv.int_,,
+        cv.Required("mode"): cv.int_,
         cv.Optional("repeat", default=1): cv.int_,
     }
 )
