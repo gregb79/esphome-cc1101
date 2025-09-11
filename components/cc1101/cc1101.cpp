4f880ca1c5379c758aa29f3a3e7602d64a21ec72
gregb79 <gregbassett@bigpond.com>
2025-09-11 15:56:27 +1000

    Update cc1101.cpp

--- components/cc1101/cc1101.cpp
+++ components/cc1101/cc1101.cpp
@@ -245,40 +245,40 @@ void CC1101::set_defaults()
 {
 
   this->set_mode(false);
-  this->write_register(CC1101_IOCFG2, 0x0D);         // GDO2 Output Pin Configuration
+  this->write_register(CC1101_IOCFG2, 0x29);         // GDO2 Output Pin Configuration
   this->write_register(CC1101_IOCFG1, 0x2E);         // GDO1 Output Pin Configuration
-  this->write_register(CC1101_IOCFG0, 0x06);         // GDO0 Output Pin Configuration
+  this->write_register(CC1101_IOCFG0, 0x0D);         // GDO0 Output Pin Configuration
   this->write_register(CC1101_FIFOTHR, 0x47);        // RX FIFO and TX FIFO Thresholds
   this->write_register(CC1101_SYNC1, 0xD3);          // Sync Word, High Byte
   this->write_register(CC1101_SYNC0, 0x91);          // Sync Word, Low Byte
-  this->write_register(CC1101_PKTLEN, 0x00);         // Packet Length
+  this->write_register(CC1101_PKTLEN, 0x1E);         // Packet Length
   this->write_register(CC1101_PKTCTRL1, 0x04);       // Packet Automation Control
   this->write_register(CC1101_PKTCTRL0, 0x32);       // Packet Automation Control
   this->write_register(CC1101_ADDR, 0x00);           // Device Address
   this->write_register(CC1101_CHANNR, 0x00);         // Channel Number
   this->write_register(CC1101_FSCTRL1, 0x06);        // Frequency Synthesizer Control
-  this->write_register(CC1101_FSCTRL0, 0x23);        // Frequency Synthesizer Control
+  this->write_register(CC1101_FSCTRL0, 0x00);        // Frequency Synthesizer Control
   this->write_register(CC1101_FREQ2, 0x10);          // Frequency Control Word, High Byte
   this->write_register(CC1101_FREQ1, 0xB0);          // Frequency Control Word, Middle Byte
   this->write_register(CC1101_FREQ0, 0x71);          // Frequency Control Word, Low Byte
-  this->write_register(CC1101_MDMCFG4, 0x87);        // Modem Configuration
-  this->write_register(CC1101_MDMCFG3, 0x93);        // Modem Configuration
+  this->write_register(CC1101_MDMCFG4, 0x67);        // Modem Configuration
+  this->write_register(CC1101_MDMCFG3, 0x32);        // Modem Configuration
   this->write_register(CC1101_MDMCFG2, 0x30);        // Modem Configuration
-  this->write_register(CC1101_MDMCFG1, 0x02);        // Modem Configuration
-  this->write_register(CC1101_MDMCFG0, 0xF8);        // Modem Configuration
-  this->write_register(CC1101_DEVIATN, 0x47);        // Modem Deviation Setting
+  this->write_register(CC1101_MDMCFG1, 0x00);        // Modem Configuration
+  this->write_register(CC1101_MDMCFG0, 0x00);        // Modem Configuration
+  this->write_register(CC1101_DEVIATN, 0x14);        // Modem Deviation Setting
   this->write_register(CC1101_MCSM2, 0x07);          // Main Radio Control State Machine Configuration
   this->write_register(CC1101_MCSM1, 0x30);          // Main Radio Control State Machine Configuration
   this->write_register(CC1101_MCSM0, 0x18);          // Main Radio Control State Machine Configuration
   this->write_register(CC1101_FOCCFG, 0x16);         // Frequency Offset Compensation Configuration
-  this->write_register(CC1101_BSCFG, 0x1C);          // Bit Synchronization Configuration
-  this->write_register(CC1101_AGCCTRL2, 0xC7);       // AGC Control
+  this->write_register(CC1101_BSCFG, 0x6C);          // Bit Synchronization Configuration
+  this->write_register(CC1101_AGCCTRL2, 0x03);       // AGC Control
   this->write_register(CC1101_AGCCTRL1, 0x00);       // AGC Control
-  this->write_register(CC1101_AGCCTRL0, 0xB2);       // AGC Control
+  this->write_register(CC1101_AGCCTRL0, 0x40);       // AGC Control
   this->write_register(CC1101_WOREVT1, 0x87);        // High Byte Event0 Timeout
   this->write_register(CC1101_WOREVT0, 0x6B);        // Low Byte Event0 Timeout
-  this->write_register(CC1101_WORCTRL, 0xF8);        // Wake On Radio Control
-  this->write_register(CC1101_FREND1, 0x56);         // Front End RX Configuration
+  this->write_register(CC1101_WORCTRL, 0xFB);        // Wake On Radio Control
+  this->write_register(CC1101_FREND1, 0xB6);         // Front End RX Configuration
   this->write_register(CC1101_FREND0, 0x11);         // Front End TX Configuration
   this->write_register(CC1101_FSCAL3, 0xE9);         // Frequency Synthesizer Calibration
   this->write_register(CC1101_FSCAL2, 0x2A);         // Frequency Synthesizer Calibration
@@ -288,7 +288,7 @@ void CC1101::set_defaults()
   this->write_register(CC1101_RCCTRL0, 0x00);        // RC Oscillator Configuration
   this->write_register(CC1101_FSTEST, 0x59);         // Frequency Synthesizer Calibration Control
   this->write_register(CC1101_PTEST, 0x7F);          // Production Test
-  this->write_register(CC1101_AGCTEST, 0x3C);        // AGC Test
+  this->write_register(CC1101_AGCTEST, 0x3F);        // AGC Test
   this->write_register(CC1101_TEST2, 0x81);          // Various Test Settings
   this->write_register(CC1101_TEST1, 0x35);          // Various Test Settings
   this->write_register(CC1101_TEST0, 0x09);          // Various Test Settings
